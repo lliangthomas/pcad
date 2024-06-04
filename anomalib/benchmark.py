4696aa7000ffa3baa9bef6186eaddb4df7f78c9b
Thomas Liang <lliang.thomas@gmail.com>
2024-06-04 09:25:25 -0500

    included statistics

--- anomalib/benchmark.py
+++ anomalib/benchmark.py
@@ -7,25 +7,31 @@ from anomalib.metrics import AUPRO, AUROC
 
 import sys
 import argparse
+import os
+import torch
 
 classnames = {
-              "bus_coppler_green": ["defect-exchange-rails-cl-cr", "defect-exchange-rails-l-cr",
+              "bus_coppler_green": ["defect-exchange-rails-l-cr", "defect-exchange-rails-cl-cr",
                                     "defect-mount-clipper", "defect-rail-cr", "defect-rail-l"], 
+              "bus_coppler_gray" : ["defect-exchange-rails-l-cr", "defect-exchange-rails-cl-cr",
+                                    "defect-mount-clipper", "defect-rail-cr", "defect-rail-l"]
             #   "router_firewall": ["defect-bottom-right", "defect-bottom-left", "defect-top-right", "defect-top-left"],
             #   "switch_8_port": [""], 
             #   "switch_16_port": [""], 
-            #   "bus_coppler_gray" : [""]
 }
 SEED = 0
+ROOT_DATA = ""
+results = {}
 
-def benchmark_model(model, task=TaskType.CLASSIFICATION, train_batch_size=32):
-    engine = Engine()
+def benchmark_model(model_name, model, train_batch_size=32, task=TaskType.CLASSIFICATION):
     for cur_class in classnames.keys():
+        engine = Engine()
         for cur_anomaly in classnames[cur_class]:
+            # if cur_anomaly == "defect-rail-cr": pass
             if task == TaskType.SEGMENTATION or task == TaskType.CLASSIFICATION:
                 print(f"Class: {cur_class}, Anomaly: {cur_anomaly}")
                 datamodule = Folder(name=cur_class,
-                                    root=f"/workspace/data/{cur_class}",
+                                    root=os.path.join(ROOT_DATA, cur_class),
                                     normal_dir="train/good",
                                     abnormal_dir=f"test/{cur_anomaly}",
                                     mask_dir=f"ground_truth/{cur_anomaly}",
@@ -37,24 +43,38 @@ def benchmark_model(model, task=TaskType.CLASSIFICATION, train_batch_size=32):
                 engine.fit(datamodule=datamodule, model=model)
                 predict = engine.predict(datamodule=datamodule, model=model)
                                          # ckpt_path=f"results/experimental")
-                print(len(predict)) 
-                for i in range(len(predict)):
-                    print(predict[i].keys())
-                #print(predict[1])
-                i, test_data = next(enumerate(datamodule.test_dataloader()))
-                print(test_data.keys())
-                print(test_data['label'])
                 aupro = AUPRO()
-                #print("IMAGE AUPRO: ", aupro(test_data['label'], predict['label']))
                 auroc = AUROC()
-                #print("IMAGE AUROC: ", auroc(test_data['label'], predict['label']))
-                return
+                all_labels = []
+                all_pred_labels = []
+                all_masks = []
+                all_pred_masks = []
+                # print(predict[0].keys())
+
+                for batch in predict:
+                    # Loop through batches
+                    all_masks.extend(batch['mask'].tolist())
+                    all_pred_masks.extend(batch['pred_masks'].tolist())
+                    all_labels.extend(batch['label'].tolist())
+                    # print(batch['label'])
+                    all_pred_labels.extend(batch['pred_labels'].tolist())
+
+                all_masks = torch.tensor(all_masks, dtype=int)
+                all_pred_masks = torch.tensor(all_pred_masks, dtype=torch.float32)
+                all_labels = torch.tensor(all_labels, dtype=int)
+                all_pred_labels = torch.tensor(all_pred_labels, dtype=torch.float32)
+
+                class_dict = results.get(cur_class, {})
+
+                class_dict[cur_anomaly] = {} 
+                class_dict[cur_anomaly]["PIXEL AUPRO"] = aupro(all_pred_masks, all_masks).item()
+                class_dict[cur_anomaly]["PIXEL AUROC"] = auroc(all_pred_masks, all_masks).item()
+                class_dict[cur_anomaly]["IMAGE AUROC"] = auroc(all_pred_labels, all_labels).item()
+                results[cur_class] = class_dict
+    print(results)
 
 
-
-def benchmark_anomalib(model=None, task = TaskType.CLASSIFICATION):
-    print("Benchmark Anomalib")
-
+def benchmark_anomalib(model_name, model=None, task=TaskType.CLASSIFICATION):
     # reconstruction_models = {Models.Ganomaly, Models.Draem, Models.ReverseDistillation}
     # feature_models = {Models.Patchcore, Models.Stfpm, Models.UflowModel, Models.Cflow, Models.EfficientAd, Models.Csflow, Models.Padim}
     
@@ -66,21 +86,26 @@ def benchmark_anomalib(model=None, task = TaskType.CLASSIFICATION):
         models = {model}
 
     for cur_model in models:
-        model_name = str(cur_model)[7:]
         print(f"Testing model: {model_name}", flush=True)
         model = cur_model()
-        benchmark_model(model)
+        train_batch_size = 32
+        if model_name == "EfficientAd":
+            train_batch_size = 1
+        benchmark_model(model_name, model, train_batch_size)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Anomalib benchmark')
     parser.add_argument('--model', dest='model', type=str, help="Model to train.")
+    parser.add_argument('--data', dest='root_data', type=str, help="Root data directory for anomalib")
 
     args = parser.parse_args()
     model = None
+    ROOT_DATA = args.root_data
+
     try:
         model = eval(f"Models.{args.model}")
     except Exception as e:
         print("Model not valid. Use one from anomalib.")
         print(e, file=sys.stderr)
     if model:
-        benchmark_anomalib(model)
+        benchmark_anomalib(args.model, model)
